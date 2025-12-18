cd204f3a590b247b069d78d9dcb65fca221e40e8
Bastian Desktop-WSL <bastian.kristensen@outlook.com>
2025-12-18 01:35:15 +0100

    high width calculations

--- QTM/results/generate_structure.py
+++ QTM/results/generate_structure.py
@@ -1,4 +1,5 @@
-from utils.structure import build_electrode, build_nanoribbon, build_reduced_device, infer_centersize
+from utils.structure import (build_electrode, build_nanoribbon, build_reduced_device, 
+                             infer_centersize, find_nearest_atoms)
 from utils.energies import hamiltonian, multi_LDOS
 
 from tqdm.auto import tqdm
@@ -31,6 +32,7 @@ BASE_DIR = PROJECT_ROOT / "results" # QTM/results
 
 def main(LWC_combinations):
     for L, W, C in tqdm(LWC_combinations, desc="LWC combinations", total=len(LWC_combinations), leave=True):
+        
         folder = BASE_DIR / f"L{L}_W{W:02}_C{C:02}"
         folder.mkdir(parents=True, exist_ok=True) # create parameter folder - also create parents necessary
         tqdm.write(f"Saving data to folder : {folder}")
@@ -40,20 +42,31 @@ def main(LWC_combinations):
         ribbon = build_nanoribbon(electrode=electrode, center_size=C)
         device, lr_idx = build_reduced_device(ribbon, electrode, repeat=3)
         
+        if W > 33: # large widths : only consider 6 center sites for LDOS
+            num_sites = 6
+            sites = find_nearest_atoms(device.xyz, device.center(), neighbours=num_sites)
+            tqdm.write(f"Large width W={W} detected, limiting LDOS calculation to {num_sites} center sites.")
+            tqdm.write(f"  -> LDOS will be computed for {num_sites} center sites: {sites}")
+        else:
+            sites = None
+        
         # Save structures
         electrode.write(folder / "electrode.xyz")
         ribbon.write(folder / "ribbon.xyz")
         device.write(folder / "device.xyz")
         
         ldos_by_C = {}
+        LDOS_E0 = None
         for modC in tqdm(C_PARMS, desc="Modulation parameter", leave=False):
             # print(f"  -> Computing LDOS for modulated_SE={modC}")
-            ldos_by_C[modC] = multi_LDOS(device=device,
+            ldos_by_C[modC], LDOS_E0 = multi_LDOS(device=device,
                                          electrode=electrode,
                                          lr_indices=lr_idx,
                                          energies=ENERGIES,
                                          Nk=Nk, eta=1e-5, form="csc",
                                          modulate_SE=modC,
+                                         LDOS_E0=LDOS_E0,
+                                         sites=sites
                                          )
 
         save_dict = {
@@ -77,8 +90,8 @@ def main(LWC_combinations):
 
 
 if __name__ == "__main__":
-    Llist = range(1,3)
-    ilist = range(0,8)
+    Llist = range(1,2)
+    ilist = range(9,12)
     all_inputs = product(Llist, ilist)
     unique_combinations = set(infer_centersize(l, i) for l, i in all_inputs)
     
